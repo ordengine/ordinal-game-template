bee6a4c2e956d71c9f5e9428943e60e0a850a464
🟧 ord.engine <168103355+ordengine@users.noreply.github.com>
2025-04-21 15:22:50 -0400

    init

---
+++ src/systems/core/RenderSystem.jsx
@@ -0,0 +1,26 @@
+import { useFrame } from "@react-three/fiber";
+import { useEntities } from "miniplex-react";
+import { ECS } from "../../modules/state.jsx";
+
+export const RenderSystem = () => {
+    const renderableEntities = useEntities(ECS.world.with("render"));
+
+    useFrame(() => {
+        for (const entity of renderableEntities) {
+            const { render, position, rotation, scale } = entity;
+            if (render?.group?.current) {
+                if (position) {
+                    render.group.current.position.set(position.x, position.y, position.z);
+                }
+                if (rotation) {
+                    render.group.current.rotation.set(rotation.x, rotation.y, rotation.z);
+                }
+                if (scale) {
+                    render.group.current.scale.set(scale.x, scale.y, scale.z);
+                }
+            }
+        }
+    });
+
+    return null
+};
